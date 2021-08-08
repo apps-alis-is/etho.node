dd42fde24f3c121d1b2dbc20fc6412d1add6b6a3
cryi <cryi@tutanota.com>
2021-08-08 22:34:39 +0200

    support for network isolation

--- src/__etho/configure.lua
+++ src/__etho/configure.lua
@@ -18,13 +18,38 @@ ami_assert(_ok, "Failed to get " .. _user .. "uid - " .. (_uid or ""))
 local _ok, _error = fs.safe_chown(DATA_PATH, _uid, _uid, {recurse = true})
 ami_assert(_ok, "Failed to chown " .. DATA_PATH .. " - " .. (_error or ""))
 
+if am.app.get_config("OUTBOUND_ADDR") ~= nil then 
+	log_info"OUTBOUND_ADDR specified. Downloading netns-cli..."
+	local _tmpFile = os.tmpname()
+    local _ok, _error = net.safe_download_file("https://github.com/alis-is/netns-cli/releases/download/0.0.1/netns-cli.lua", _tmpFile, {followRedirects = true})
+    if not _ok then
+        fs.remove(_tmpFile)
+        ami_error("Failed to download: " .. tostring(_error))
+    end
+	fs.move(_tmpFile, "bin/netns-cli.lua")
+	log_success"netns-cli downloaded"
+end
+
 log_info "Configuring ETHO FS..."
 local GETH_PATH = '"' .. path.combine("bin", "geth") .. '"'
 
 local _home = env.get_env("HOME")
 env.set_env("HOME", DATA_PATH)
-ami_assert(os.execute(GETH_PATH .. " --ethofs=" .. am.app.get_config("NODE_TYPE") .. " --ethofsInit"), "Failed to initialize ETHO FS")
-ami_assert(os.execute(GETH_PATH .. " --ethofs=" .. am.app.get_config("NODE_TYPE") .. " --ethofsConfig"), "Failed to configure ETHO FS")
+if am.app.get_config("OUTBOUND_ADDR") ~= nil then 
+	local _netnsId = am.app.get("id") .. "-netns"
+	ami_assert(os.execute("eli bin/netns-cli.lua --force --id=" .. _netnsId .. " --outbound-addr=" .. am.app.get_config("OUTBOUND_ADDR")), "Failed to create netns!")
+	local _ok = os.execute("ip netns exec " .. _netnsId .. " " .. GETH_PATH .. " --ethofs=" .. am.app.get_config("NODE_TYPE") .. " --ethofsInit")
+	if _ok then 
+		_ok = os.execute("ip netns exec " .. _netnsId .. " " ..GETH_PATH .. " --ethofs=" .. am.app.get_config("NODE_TYPE") .. " --ethofsConfig")
+	end
+	os.execute("eli bin/netns-cli.lua --id=" .. am.app.get("id") .. " --remove")
+	if not _ok then 
+		ami_error("Failed to initialize ETHO FS")
+	end
+else
+	ami_assert(os.execute(GETH_PATH .. " --ethofs=" .. am.app.get_config("NODE_TYPE") .. " --ethofsInit"), "Failed to initialize ETHO FS")
+	ami_assert(os.execute(GETH_PATH .. " --ethofs=" .. am.app.get_config("NODE_TYPE") .. " --ethofsConfig"), "Failed to configure ETHO FS")
+end
 env.set_env("HOME", _home)
 
 log_success "ETHO FS configured."
@@ -35,8 +60,3 @@ am.execute_extension("__eth/configure.lua")
 
 log_success "ETHO services configured"
 
---[[
-      /usr/sbin/geth --ethofs=gn --ethofsInit
-
-  /usr/sbin/geth --ethofs=gn --ethofsConfig
-]]
--- src/model.ext.lua
+++ src/model.ext.lua
@@ -5,7 +5,8 @@ am.app.set_model(
         SERVICE_NAME = "etho-geth",
         SERVICE_FILE = "__etho/assets/daemon.service",
         IPC_PATH = path.combine(_dataDir, ".ether1/geth.ipc"),
-        CHAINDATA_DIR = path.combine(_dataDir, ".ether1/geth/chaindata/")
+        CHAINDATA_DIR = path.combine(_dataDir, ".ether1/geth/chaindata/"),
+        IS_GN = am.app.get_configuration("NODE_TYPE", ""):match("gn")
     },
     { merge = true, overwrite = true }
 )
