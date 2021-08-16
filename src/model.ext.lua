44f6fd4838adff037701750a877865eeb1a4e221
cryi <cryi@tutanota.com>
2021-08-16 21:40:22 +0200

    fixes for ubuntu 16

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,12 +1,19 @@
 local _dataDir = path.combine(os.cwd(), "data")
 
+local _which = proc.exec("which ip", { stdout = "pipe" })
+IP_PATH="/usr/sbin/ip"
+if _which.exitcode == 0 and _which.stdoutStream ~= nil then 
+    IP_PATH = _which.stdoutStream:read("a"):gsub("^%s*(.-)%s*$", "%1")
+end
+
 am.app.set_model(
     {
         SERVICE_NAME = "etho-geth",
         SERVICE_FILE = "__etho/assets/daemon.service",
         IPC_PATH = path.combine(_dataDir, ".ether1/geth.ipc"),
         CHAINDATA_DIR = path.combine(_dataDir, ".ether1/geth/chaindata/"),
-        IS_GN = am.app.get_configuration("NODE_TYPE", ""):match("gn")
+        IS_GN = am.app.get_configuration("NODE_TYPE", ""):match("gn"),
+        IP_PATH = IP_PATH
     },
     { merge = true, overwrite = true }
 )
